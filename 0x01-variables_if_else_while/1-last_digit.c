e7643763417b8aec3730f72dff171decc1677b21
juancajes <juancajesma@gmail.com>
2022-02-08 13:58:30 -0800

    space was corrected

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,11 +20,11 @@ int main(void)
 	{
 		printf("%d is %d and is greater than 5\n", n, n % 10);
 	}
-	if (n % 10 == 0)
+	else if (n % 10 == 0)
 	{
 		printf("%d is %d and is 0\n", n, n % 10);
 	}
-	if (n % 10 < 6)
+	else if (n % 10 < 6)
 	{
 		printf("%d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
