3c16eaafdcc13916d651c6eed0d38aa5253acedc
juancajes <juancajesma@gmail.com>
2022-02-08 14:21:16 -0800

    updating files 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print the variable
+ * Return:0
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n < 123)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
