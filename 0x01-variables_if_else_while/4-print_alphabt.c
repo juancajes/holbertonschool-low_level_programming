69582d7cd0d6e082d9c6e0c58582ea9077396989
juancajes <juancajesma@gmail.com>
2022-02-08 19:12:53 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		if (n == 101 || n == 113)
+		{
+		}
+		else
+		{
+			putchar(n);
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
