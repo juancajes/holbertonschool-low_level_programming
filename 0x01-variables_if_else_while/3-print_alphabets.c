695b1b9d1aa66dd5cb87d559cf7dbbd9006efc16
juancajes <juancajesma@gmail.com>
2022-02-08 18:51:50 -0800

    updating files 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 97; i < 122; i++)
+	for (i = 97; i < 123; i++)
 	{
 		putchar(i);
 	}
-	for (i = 65; i < 90; i++)
+	for (i = 65; i < 91; i++)
 	{
 		putchar(i);
 	}
